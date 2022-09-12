d1dd4c885bfb40ca6a2a43dfebcc9709ae1d07ea
Sanhe <MacHu-GWU@users.noreply.github.com>
2022-09-11 22:56:42 -0400

    c

--- wotlkdoc/docs/gps/class_skill_trainer.py
+++ wotlkdoc/docs/gps/class_skill_trainer.py
@@ -13,8 +13,13 @@ from .go_cmd import with_teleport_command
 def lt_list_class_trainer_gsp():
     reader = TsvGzReader(__file__)
     df = reader.read_df("class-skill-trainer.tsv.gz")
-    columns = "阵营,职业,城市,位置,go_cmd".split(",")
-    df.columns = columns
+    df = df.select([
+        pl.col("faction").alias("阵营"),
+        pl.col("class").alias("职业"),
+        pl.col("city").alias("城市"),
+        pl.col("loc").alias("位置"),
+        pl.col("go_cmd"),
+    ])
     df1 = with_teleport_command(df, go_cmd_col="go_cmd")
     df2 = df1.with_column(pl.col("阵营").apply(f=image_by_faction))
     df3 = df2.with_column(pl.col("职业").apply(f=image_by_class))
--- wotlkdoc/docs/gps/main_city.py
+++ wotlkdoc/docs/gps/main_city.py
@@ -19,16 +19,18 @@ def lt_list_main_city_gps_and_label_and_image() -> T.List[
 ]:
     reader = TsvGzReader(__file__)
     df = reader.read_df("main-city.tsv.gz")
-
-    columns = "城市,地点,描述,go_cmd".split(",")
-    df.columns = columns
+    df = df.select([
+        pl.col("zone").alias("城市"),
+        pl.col("zone").alias("图标"),
+        pl.col("loc_name").alias("地点"),
+        pl.col("description").alias("描述"),
+        pl.col("go_cmd").alias("go_cmd"),
+    ])
     df1 = with_teleport_command(df, go_cmd_col="go_cmd")
-    df2 = df1.with_column(pl.col("城市").apply(f=icon_by_portal).alias("图标"))
-    df3 = df2.select("图标,城市,地点,描述,传送命令1,传送命令2".split(","))
-
+    df2 = df1.with_column(pl.col("图标").apply(f=icon_by_portal))
     lst = list()
-    for city in df3["城市"].unique(maintain_order=True):
-        sub_df = df3.filter(df3["城市"] == city)
+    for city in df2["城市"].unique(maintain_order=True):
+        sub_df = df2.filter(df2["城市"] == city)
         image = image_by_map(city)
         image.height = 480
         lst.append(
