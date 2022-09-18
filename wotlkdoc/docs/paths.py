6c34579ce54f47cec45e4a1d437c0956bce2cf1a
Sanhe <MacHu-GWU@users.noreply.github.com>
2022-09-18 15:07:01 -0400

    Update paths.py

--- wotlkdoc/docs/paths.py
+++ wotlkdoc/docs/paths.py
@@ -4,13 +4,18 @@
 Doc related file / folder paths
 """
 
-from pathlib_mate import Path
 import os
-for k, v in os.environ.items():
-    print(k, v)
-print(os.getcwd())
-print(os.listdir(os.getcwd()))
-dir_project_root = Path.dir_here(__file__).parent.parent
+from pathlib_mate import Path
+
+
+if "READTHEDOCS" in os:
+    dir_project_root = Path.cwd().parent.parent
+else:
+    dir_project_root = Path.dir_here(__file__).parent.parent
+
+print("*******")
+print(dir_project_root)
+print(os.listdir(str(dir_project_root)))
 assert (dir_project_root / "setup.py").exists()
 
 dir_docs = dir_project_root / "docs"
